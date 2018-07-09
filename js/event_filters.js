bfdb14edc2dc788e0cfef553f5b2914d4ec94eee
Bohdan Koldun <38987744+bohdan-koldun@users.noreply.github.com>
2018-07-10 00:29:32 +0300

    clear input search

--- js/event_filters.js
+++ js/event_filters.js
@@ -1,16 +1,17 @@
 
 //uploads date filter
 let dateSelect = document.getElementById('date_filter');
+let searchFilter = document.getElementById('search_filter');
 
 dateSelect.onchange = function () {
     let dateFilter = dateSelect.options[dateSelect.selectedIndex].value;
     localStorage.setItem('date_filter', dateFilter);
+    searchFilter.value = '';
 
-    let searchFilter = localStorage.getItem('search_filter');
     let tagsFilter = localStorage.getItem('tags_filter');
     if(tagsFilter == '') tagsFilter = [];
-     else tagsFilter = tagsFilter.split(",");
-    loadJSON(dateFilter, tagsFilter, searchFilter) 
+    else tagsFilter = tagsFilter.split(",");
+    loadJSON(dateFilter, tagsFilter, '') 
  
 };
 
@@ -21,15 +22,13 @@ tagsFilter.onchange = function () {
     let tagsSpan = document.getElementsByClassName('tag');
     tags = [];
     localStorage.setItem('tags_filter', tags);
+    searchFilter.value
 
     let dateFilter = localStorage.getItem('date_filter');
-    let searchFilter = localStorage.getItem('search_filter');
-    loadJSON(dateFilter, tags, searchFilter) 
+    loadJSON(dateFilter, tags,'' ) 
 }
 
 //search filter
-let searchFilter = document.getElementById('search_filter');
-
 searchFilter.onkeyup = function () {
 
     let keyword = searchFilter.value;
